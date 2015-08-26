732ad056c9ad844edc2a10d504aba2453a2e13a7
RK905 <mr.rayenk@gmail.com>
2015-08-26 12:42:46 -0400

    Swift Stop Watch
    NSTIMER

--- SwiftStopWatch/ViewController.swift
+++ SwiftStopWatch/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TimerLabel: UILabel!
+    
+    var timer = NSTimer()
+    var count = 0
+    func updateTime(){
+        count++
+        TimerLabel.text = "\(count)"
+        
+    }
+    
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func stopBtn() {
+       timer.invalidate()
+    }
+    
+    
+    
+    @IBAction func startBtn() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self , selector : Selector("updateTime"), userInfo: nil, repeats : true)
+    }
+    
+    
+    @IBAction func resetBtn() {
+        timer.invalidate()
+        count = 0
+        TimerLabel.text = "0"
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
